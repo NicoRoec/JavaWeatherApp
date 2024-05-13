2642fc6ae9559e78b7da83ae26641dbc4b5e4888
NicoRoec <nico.roecker@reservix.de>
2024-05-13 17:34:47 +0200

    Added searchbar and searchbutton; added search.png

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -1,5 +1,10 @@
 package src;
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class WeatherAppGui extends JFrame {
     public WeatherAppGui(){
@@ -18,7 +23,44 @@ public class WeatherAppGui extends JFrame {
         // making layout manager null to manually position components within the gui
         setLayout(null);
 
-        //prevent any resize of gui
+        // prevent any resize of gui
         setResizable(false);
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents(){
+        // search field
+        JTextField searchTextField = new JTextField();
+
+        // set location and size of component
+        searchTextField.setBounds(15, 15, 351, 45);
+
+        // change font
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+        add(searchTextField);
+
+        // search button
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+
+        //change cursor to hand cursor when hover
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+    }
+
+    private ImageIcon loadImage(String resourcePath){
+        try{
+            // read Img file from given path
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            // returns image icon that component can read it
+            return new ImageIcon(image);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Resource konnte nicht gefunden werden.");
+        return null;
     }
 }
